7ee547dfb38a57df33b7ea6e508e9ad17b2639de
rzv91 <pgl_razvan@yahoo.com>
2014-09-07 22:07:24 +0300

    Model de baza User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -98,6 +98,17 @@ describe "with invalid password" do
 it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
 
+describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
+
   end
 end
 
