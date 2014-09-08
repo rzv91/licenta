a6f6a6ad604344e06376ca2b1221256530f2692c
rzv91 <pgl_razvan@yahoo.com>
2014-09-09 00:33:41 +0300

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ def new
 def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the application!  Registration succeded !"
       redirect_to @user
     else
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,10 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+
 
   it { should be_valid }
 
@@ -111,6 +115,12 @@ describe "email address with mixed case" do
 
   end
 end
+
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
 end
 
